d372eddea20a9f97684c6256375e3e29674c7556
HsXDean <deanhs0dean@gmail.com>
2022-04-02 00:04:57 +0800

    1

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,67 +1,61 @@
 ﻿#include <iostream>
+#include <string.h>
 using namespace std;
 
-template<class T>
 // ChangeSize1D
+template<class T>
 void ChangeSize1D(T*& a, const int oldSize, const int newSize) {
-	if (newSize < 0) throw"New length must be >= 0";
-
+	//if (newSize < 0) throw"New length must be >= 0";
 	T* temp = new T[newSize];
 	int number = min(oldSize, newSize);
 	copy(a, a + number, temp);
 	delete[] a;
 	a = temp;
 }
+
 template<class T>
 class Stack {
-public:
-	Stack(int stackCapacity);
-	~Stack() { delete[] stack; }
-	bool IsEmpty()const;
-	T& Top() const;
-	void Push(const T& item);
-	void Pop();
 private:
-	T* stack;     // array for stack elements
-	int top;      // position of top element
-	int capacity; // capacity of stack array 
-};
-// constructor
-template<class T>
-Stack<T>::Stack(int stackCapacity) : capacity(stackCapacity)
-{
-	if (capacity < 1) throw "Stack capacity must be > 0";
-	stack = new T[capacity];
-	top = -1;
-}
-// IsEmpty
-template<class T>
-inline bool Stack<T>::IsEmpty()const { return top == -1; }
-// Top
-template<class T>
-inline T& Stack<T>::Top()const {
-	if (IsEmpty()) throw"Stack is empty!";
-	return stack[top];
-}	
-// Push
-template<class T>
-void Stack<T>::Push(const T& x) {
-	if (top == capacity - 1) {
-		ChangeSize1D(stack, capacity, 2 * capacity);
-		capacity *= 2;
+	T* stack;
+	int top;
+	int capacity;
+public:
+	Stack(int stackCapacity) : capacity(stackCapacity) {
+		if (capacity < 1) throw "Stack capacity must be > 0";
+		stack = new T[capacity];
+		top = -1;
 	}
-	stack[++top] = x;
-}
-// Pop
-template<class T>
-void Stack<T>::Pop() {
-	if (IsEmpty()) throw"Stack is empty. Cannot delete.";
-	stack[top--].~T();
-}
+	~Stack() {
+		delete[] stack;
+		top = capacity = 0;
+	}
+	bool IsEmpty() const{
+		return top == -1;
+	}
+	T& Top() const {
+		if (IsEmpty()) throw"Stack is empty!";
+		return stack[top];
+	}
+	void Push(const T & data) {
+		if (top == capacity - 1) {
+			ChangeSize1D(stack, capacity, 2 * capacity);
+			capacity *= 2;
+		}
+		stack[++top] = data;
+	}
+	T Pop() {
+		if (IsEmpty()) throw "Stack is empty. Cannot delete.";
+		T t = Top();
+		stack[top--].~T();
+		return t;
+	}
+};
 
 
 int main()
 {
+	// test
+	/*
 	int n;
 	cin >> n;
 	Stack<int> a(n);
@@ -70,9 +64,9 @@ int main()
 		cin >> x;
 		a.Push(x);
 	}
-	for (int i = n - 1; i >= 0; i--) {
-		cout << a.Top() << " ";
-		a.Pop();
+	for (int i = 0; i < n; i++) {
+		cout << a.Pop() << endl;
 	}
+	*/
 }
 
