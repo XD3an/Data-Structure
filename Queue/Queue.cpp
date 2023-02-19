278075335a7261790fa9cbe3fdf6e3e0fe8824da
XD3an <99171846+XD3an@users.noreply.github.com>
2023-02-19 15:14:38 +0800

    update Queue.cpp

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 template<class T>
-void ChangeSize1D(T*& a, const int oldSize, int newSize) {
+void ChangeSize1D(T*& a, const int oldSize, int newSize) 
+{
     if (newSize < 0) throw "New length must be >= 0";
     T* temp = new T[newSize];
     int number = min(oldSize, newSize);
@@ -10,8 +11,10 @@ void ChangeSize1D(T*& a, const int oldSize, int newSize) {
     delete[] a;
     a = temp;
 }
+
 template<class T>
-class Queue {
+class Queue 
+{
 private:
     T* queue;
     int front, rear, capacity;
@@ -27,7 +30,8 @@ public:
 
 // Queue
 template<class T>
-Queue<T>::Queue(int queueCapacity) : capacity(queueCapacity) {
+Queue<T>::Queue(int queueCapacity) : capacity(queueCapacity) 
+{
     if (queueCapacity < 1) throw "Queue capacity must be > 0";
     queue = new T[queueCapacity];
     front = rear = -1;
@@ -35,35 +39,41 @@ Queue<T>::Queue(int queueCapacity) : capacity(queueCapacity) {
 
 // ~Queue
 template<class T>
-Queue<T>::~Queue() {
+Queue<T>::~Queue() 
+{
     delete[] queue;
     front = rear = capacity;
 }
 
 //IsEmpty
 template<class T>
-bool Queue<T>::IsEmpty() const{
+bool Queue<T>::IsEmpty() const
+{
     return front == rear;
 }
 
 // Front
 template<class T>
-T& Queue<T>::Front() const{
+T& Queue<T>::Front() const
+{
     if (IsEmpty()) throw "Queue is empty. No fornt element.";
     return queue[front+1];
 }
 
 // Rear
 template<class T>
-T& Queue<T>::Rear() const {
+T& Queue<T>::Rear() const 
+{
     if (IsEmpty()) throw "Queue is empty. No rear element.";
     return queue[rear];
 }
 
 // Push
 template<class T>
-void Queue<T>::Push(const T& data){
-    if (rear == capacity - 1) {
+void Queue<T>::Push(const T& data)
+{
+    if (rear == capacity - 1) 
+    {
         ChangeSize1D(queue, capacity, 2 * capacity);
         capacity *= 2;
     }
@@ -72,7 +82,8 @@ void Queue<T>::Push(const T& data){
 
 // Pop
 template<class T>
-void Queue<T>::Pop() {
+void Queue<T>::Pop() 
+{
     if (IsEmpty()) throw "Queue is Empty, Cannot delete";
     for (int i = front+1; i < rear; i++) {
         queue[i] = queue[i + 1];
@@ -83,19 +94,23 @@ void Queue<T>::Pop() {
 int main()
 {
    // test
-   /*
     int n;
+    // Input
+    cout << "Size:";
     cin >> n;
     Queue<int> q(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         int t;
         cin >> t;
         q.Push(t);
     }
-    cout << endl;
-    for (int i = 0; i < n; i++) {
+    
+    // Output
+    cout << "Queue:\n";
+    for (int i = 0; i < n; i++) 
+    {
         cout << q.Front() << " " << q.Rear() << endl;
         q.Pop();
     }
-    */
 }
